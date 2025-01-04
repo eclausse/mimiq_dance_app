2a4533aba80e35cb05589a088efab15da16a3e2d
Xess1407 <clausseeva@cy-tech.fr>
2025-01-04 15:24:35 +0100

    Add Navigable Pin Google Map
    Signed-off-by: Xess1407 <clausseeva@cy-tech.fr>

--- lib/views/map_view.dart
+++ lib/views/map_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mimiq_dance/models/studio_info_model.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:mimiq_dance/views/studio_info_page.dart';
 
 class MapView extends StatelessWidget {
   MapView({super.key});
@@ -11,7 +12,7 @@ class MapView extends StatelessWidget {
     studios = StudioInfoModel.getStudiosInfo();
   }
 
-  final LatLng _center = const LatLng(-33, 151);
+  final LatLng _center = const LatLng(41.850033, -87.6500523);
   
   @override
   Widget build(BuildContext context) {
@@ -30,8 +31,39 @@ class MapView extends StatelessWidget {
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
           target: _center,
-          zoom: 11.0,
+          zoom: 5.0,
         ),
+        markers: {
+          Marker(
+               markerId: MarkerId(studios[0].name),
+               position: const LatLng(42.714040,-82.589822),
+               onTap: () => {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => StudioInfoView(studioModel: studios[0]))
+                )
+              }
+            ), 
+          Marker(
+            markerId: MarkerId(studios[2].name),
+            position: const LatLng(42.792011,-90.882680),
+            onTap: () => {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => StudioInfoView(studioModel: studios[2]))
+              )
+            }
+          ), 
+          Marker(
+              markerId: MarkerId(studios[3].name),
+              position: const LatLng(35.197642,-80.787244),
+              onTap: () => {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => StudioInfoView(studioModel: studios[3]))
+              )
+            }
+            ),
+          
+      }, // markers
+
       )
     );
   }
